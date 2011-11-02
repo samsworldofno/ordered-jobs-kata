9e054f1988ac8716df1b51e71a46c5409044d6f0
Sam Phillips <sam@samdanavia.com>
2011-11-02 14:09:50 +0000

    js step four green, refactored into basic classes

--- javascript/spec/ordered_jobs.spec.js
+++ javascript/spec/ordered_jobs.spec.js
@@ -1,81 +1,127 @@
-function job_sequence(input) {
-	console.log(input)
+function Job(input){
+	var instruction;
 	
-	return input.replace(/\W*/g, '')
+	input = input.replace(/\s*/g, '');
+	instruction = input.split(/=>/);
+
+	this.name = instruction[0];
+	this.dependency = instruction[1] ? instruction[1] : null;	
 }
 
-describe("JobSequence", function() {
-
-	beforeEach(function(){	
-	  this.addMatchers({
-
-	    toContainAllOf: function(expected) {
-				for(var i = 0; i < expected.length; i++) {
-					if(this.actual.indexOf(expected[i]) < 0){
-						return false;
-					}
-				}
-				return true;
-	    }
-
-	  });
-	});
-
-	it("should return a blank string when a blank string is entered", function(){
-		var input = '';
-		expect(job_sequence(input)).toEqual('');
-  });
-
-	it("should return a single job when that single job is passed", function(){
-		var input = ' a => ';
-		expect(job_sequence(input)).toEqual('a');
-	})
+function JobList(input){
+	this.input = input;
 	
-	it("should return a multiple jobs in non-significant order when there are no dependencies", function(){
-		var input = '	a => \n\
-                  b => \n\
-                  c => ';
-
-		var result = job_sequence(input)
-		expect(result).toContainAllOf(['a', 'b', 'c'])
-	})
-	
-	it("should return multiple jobs in a significant order when dependencies exist", function() {
-	  var input = '	a => \r\
-                  b => c \r\
-                  c =>';
-
-		// result = job_sequence(input)
+	this.jobs = function(){
+		var result = [];
 		
-		var jobs = [];
-		var result = '';
-		var lines = input.split(/\r/g);
-		
+		var lines = this.input.split(/\n/g);
 		for(var i = 0; i < lines.length; i++) {
-			lines[i] = lines[i].replace(/\s*/g, '');
+			var job = new Job(lines[i]);
+			result.push(job)
+		}
 
-			var instruction = lines[i].split(/=>/);
-			var job = instruction[0];
+		return result;
+	}
+	
+	this.sequence = function(){
+		var sequence = [];
+		
+		for(var i = 0; i < this.jobs().length; i++) {
+			var job = this.jobs()[i];
 
-			if(jobs.indexOf(job) == -1) {
-				jobs.push(job);
+			if(sequence.indexOf(job.name) == -1) {
+				sequence.push(job.name);
 			}
 
-			if(instruction[1]) {
-				if(jobs.indexOf(instruction[1]) > -1) {
-					jobs.splice(jobs.indexOf(instruction[1]),1);
+			if(job.dependency) {
+				if(sequence.indexOf(job.dependency) > -1) {
+					sequence.splice(sequence.indexOf(job.dependency),1);
 				}
-				
-				var dependent_position = jobs.indexOf(job);
-				
-				jobs.splice(dependent_position, 0, instruction[1]);
-				
+
+				var dependent_position = sequence.indexOf(job.name);
+
+				sequence.splice(dependent_position, 0, job.dependency);			
 			}
 		}
 
-		result = jobs.join('');
+		return sequence.join('');		
+	}
+}
 
-		expect(result).toContainAllOf(['a', 'b', 'c'])
-		expect(result.indexOf('c')).toBeLessThan(result.indexOf('b'))
+describe("Job", function(){
+	it("should extract a job's name from the input string", function() {
+		var job = new Job("a =>");
+		expect(job.name).toEqual('a');
+	});
+	
+	it("should extract a job's dependency from the input string", function() {
+	  var job;
+
+		job = new Job("a => b");
+		expect(job.dependency).toEqual('b');
+		
+		job = new Job("a => ");
+		expect(job.dependency).toBeNull();
 	});
 });
+
+describe("JobList", function() {
+  it("should return a list of jobs from an input string", function() {
+    var list = new JobList("a => ");
+		
+		expect(list.jobs().length).toEqual(1);
+  });
+
+	describe("sequence", function(){
+		beforeEach(function(){	
+		  this.addMatchers({
+
+		    toContainAllOf: function(expected) {
+					for(var i = 0; i < expected.length; i++) {
+						if(this.actual.indexOf(expected[i]) < 0){
+							return false;
+						}
+					}
+					return true;
+		    }
+
+		  });
+		});
+
+		it("should return a blank string when a blank string is entered", function(){
+			var input = '';
+			var list = new JobList(input)
+
+			expect(list.sequence()).toEqual('');
+	  });
+
+		it("should return a single job when that single job is passed", function(){
+			var input = ' a => ';
+			var list = new JobList(input);
+
+			expect(list.sequence()).toEqual('a');
+		})
+
+		it("should return a multiple jobs in non-significant order when there are no dependencies", function(){
+			var input = '	a => \n\
+	                  b => \n\
+	                  c => ';
+			var list = new JobList(input);
+
+			expect(list.sequence()).toContainAllOf(['a', 'b', 'c']);
+		})
+
+		it("should return multiple jobs in a significant order when dependencies exist", function() {
+		  var input = '	a => \n\
+	                  b => c \n\
+	                  c =>';
+			var list = new JobList(input);
+
+	  	result = list.sequence();
+
+			expect(result).toContainAllOf(['a', 'b', 'c'])
+			expect(result.indexOf('c')).toBeLessThan(result.indexOf('b'))
+		});
+	});
+});
+
